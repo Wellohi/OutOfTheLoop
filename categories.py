4ee6fb100761a8d49654d59352757c165ec7258f
Well <wsaraivabarros@gmail.com>
2025-08-01 14:48:49 -0300

    início fase 3

--- categories.py
+++ categories.py
@@ -1,6 +1,14 @@
 # uma lista de palavras para testar o jogo
 game_words = {
-    "Animal": ['Leão', 'Pato', 'Papagaio', 'Flamingo', 'Ornitorrinco', 'Tamanduá'],
-    "Comida": ['Arroz carreteiro', 'Estrogonofe de frango', 'Macarrão', 'Panqueca', 'Almondegas', 'Bolo de cenoura com cobertura de chocolate'],
-    "Objeto": ['Grampeador', 'Relogio de pendulo', 'Canivete', 'Lanterna', 'Garrafa térmica', 'Lampião']
-}
+"Animal": [
+        'Leão', 'Pato', 'Papagaio', 'Flamingo', 'Ornitorrinco', 'Tamanduá'
+        ],
+    
+    "Comida": [
+        'Arroz carreteiro', 'Estrogonofe de frango', 'Macarrão', 'Panqueca', 'Almondegas', 'Bolo de cenoura com cobertura de chocolate'
+        ],
+    
+    "Objeto": [
+        'Grampeador', 'Relogio de pendulo', 'Canivete', 'Lanterna', 'Garrafa térmica', 'Lampião'
+        ]
+    }
---
+++ questions.py
@@ -0,0 +1,27 @@
+# uma lista de palavras para testar o jogo
+game_questions = {
+    "Animal": [
+        'Esse animal seria um bom mascote para um time de futebol?',
+        'Você acha que esse bicho faz muito barulho?',
+        'Ele caberia em uma mochila?',
+        'Esse animal poderia viver dentro de casa?',
+        'Você teria medo de encontrar um desses à noite?',
+        ],
+    
+    "Comida": [
+        'Esse prato combina mais com um almoço de domingo ou com um lanche rápido?',
+        'Ele é mais crocante ou cremoso?',
+        'Esse alimento combina com ketchup?',
+        'Você comeria isso no café da manhã?',
+        'É o tipo de comida que você divide com alguém ou come sozinho?',
+        ],
+    
+    "Objeto": [
+        'Esse objeto é fácil de perder?',
+        'Ele é mais útil dentro ou fora de casa?',
+        'Você consegue encontrar isso em um escritório?',
+        'Esse objeto pode ser considerado perigoso?',
+        'Ele precisa de eletricidade pra funcionar?',
+        ],
+    }
+
