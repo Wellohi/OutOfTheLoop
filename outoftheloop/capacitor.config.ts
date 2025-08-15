e22b3c87de75d8c8a0177eda79815fb1954c77d3
Wellohi <wsaraivabarros@gmail.com>
2025-08-15 11:33:19 -0300

    Add Capacitor configuration file

--- outoftheloop/capacitor.config.ts
+++ outoftheloop/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'outoftheloop',
-  webDir: 'dist'
+  appId: 'com.mygame.outoftheloop',
+  appName: 'OutOfTheLoop',
+  webDir: 'build'
 };
 
 export default config;
